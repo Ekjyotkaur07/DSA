2b759f7ad0633961b457d8e37e0f41b0cff5cbfc
Ekjyotkaur07 <ekjyotkaurmalhotra@gmail.com>
2026-04-06 13:30:58 +0530

    simple calculator

--- basics.cpp
+++ basics.cpp
@@ -6,15 +6,33 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int age=19;
+//     bool idcard=false;
+//     if(age&&idcard){
+//         cout<<"eligible";
+//     }
+//     else{
+//         cout<<"not eligible";
+//     }
+// }
+
 #include <iostream>
 using namespace std;
 int main(){
-    int age=19;
-    bool idcard=false;
-    if(age&&idcard){
-        cout<<"eligible";
+    char op;
+    int num1,num2;
+    cout<<"Enter op"<<endl<<"Enter num1 and num2"<<endl;
+    cin>>op;
+    cin>>num1;
+    cin>>num2;
+    switch(op){
+        case '+':cout<<"sum is "<<num1+num2<<endl; break;
+        case '-':cout<<"subtraction is "<<num1-num2<<endl; break;
+        case '*':cout<<"multiply is "<<num1*num2<<endl; break;
+        case '/':cout<<"divison"<<num1/num2<<endl; break;
     }
-    else{
-        cout<<"not eligible";
-    }
+    return 0;
 }
