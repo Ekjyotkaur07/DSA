2a0fb43707943579810678af924ab59d82ea68c9
Ekjyotkaur07 <ekjyotkaurmalhotra@gmail.com>
2026-04-06 13:25:41 +0530

    if-else program

--- basics.cpp
+++ basics.cpp
@@ -1,7 +1,20 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello everyone"<<endl;
+//     cout<<"I am Ekjyot";
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Hello everyone"<<endl;
-    cout<<"I am Ekjyot";
-    return 0;
+    int age=19;
+    bool idcard=false;
+    if(age&&idcard){
+        cout<<"eligible";
+    }
+    else{
+        cout<<"not eligible";
+    }
 }
