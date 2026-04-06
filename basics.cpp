90ad9d030ca32bec6575184daf88c00bab855a6a
Ekjyotkaur07 <ekjyotkaurmalhotra@gmail.com>
2026-04-06 13:37:08 +0530

    prime number

--- basics.cpp
+++ basics.cpp
@@ -19,20 +19,43 @@
 //     }
 // }
 
+//simple calculator
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     char op;
+//     int num1,num2;
+//     cout<<"Enter op"<<endl<<"Enter num1 and num2"<<endl;
+//     cin>>op;
+//     cin>>num1;
+//     cin>>num2;
+//     switch(op){
+//         case '+':cout<<"sum is "<<num1+num2<<endl; break;
+//         case '-':cout<<"subtraction is "<<num1-num2<<endl; break;
+//         case '*':cout<<"multiply is "<<num1*num2<<endl; break;
+//         case '/':cout<<"divison"<<num1/num2<<endl; break;
+//     }
+//     return 0;
+// }
+
+//prime or not
 #include <iostream>
 using namespace std;
 int main(){
-    char op;
-    int num1,num2;
-    cout<<"Enter op"<<endl<<"Enter num1 and num2"<<endl;
-    cin>>op;
-    cin>>num1;
-    cin>>num2;
-    switch(op){
-        case '+':cout<<"sum is "<<num1+num2<<endl; break;
-        case '-':cout<<"subtraction is "<<num1-num2<<endl; break;
-        case '*':cout<<"multiply is "<<num1*num2<<endl; break;
-        case '/':cout<<"divison"<<num1/num2<<endl; break;
+     int num1;
+     cout<<"enter num1";
+     cin>>num1;
+     bool flag=true;
+     for(int i=2;i<num1;i++){
+        if(num1%i==0){
+           flag=false;
+           break;
+        } }
+    if(flag){
+        cout<<"Prime number";
+    }
+    else{
+        cout<<"Not a prime number";
     }
     return 0;
 }
